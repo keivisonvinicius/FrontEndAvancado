b576fadef957408b60bb0e695f3d28c55f638dcf
Keivison <eikeivison@gmail.com>
2025-03-30 16:56:41 -0300

    atualizando

--- aula3/src/App.jsx
+++ aula3/src/App.jsx
@@ -6,12 +6,12 @@ const App = () => {
   return (
     <div>
       {
-        [{nome: "Keivison", email: "eikeivison@gmail.com", curso: "Sistemas de Informação"},
-          {nome: "Rafaella", email: "rafaella@gmail.com", curso: "Fisioterapia"},
-          {nome: "Diogo", email: "diogo@gmail.com", curso: "Ciencia da computação"},
-          {nome: "Laiane", email: "laiane@gmail.com", curso: "Fisioterapia"}
+        [{nome: "Keivison", email: "eikeivison@gmail.com", curso: "Sistemas de Informação",media:10},
+          {nome: "Rafaella", email: "rafaella@gmail.com", curso: "Fisioterapia", media: 2},
+          {nome: "Diogo", email: "diogo@gmail.com", curso: "Ciencia da computação", media:8},
+          {nome: "Laiane", email: "laiane@gmail.com", curso: "Fisioterapia", media: 8}
         ].map((aluno) => 
-          <Aluno nome = {aluno.nome} email = {aluno.email} curso = {aluno.curso}  /> 
+          <Aluno nome = {aluno.nome} email = {aluno.email} curso = {aluno.curso} media = {aluno.media} /> 
         )
       }
       
--- aula3/src/components/Aluno.jsx
+++ aula3/src/components/Aluno.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 
-const Aluno = ({ nome, email, curso }) => {
+const Aluno = ({ nome, email, curso, media,}) => {
+
+
+  const status = media >= 7 ? "Aprovado(a)" : "Reprovado(a)"
 
 
 
   return (
-    <div>
+    <>
         <p>Nome: {nome}</p>
         <p>Email: {email}</p>
         <p>Curso: {curso}</p>
+        <p>Média: {media}</p>
+        <p>Status: {status}</p>
         <p>----------------------</p>
-    </div>
+        </>
   )
 }
 
