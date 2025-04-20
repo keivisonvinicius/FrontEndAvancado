d3fffdec77c2cf7716e8737586abd90118253bda
Keivison <eikeivison@gmail.com>
2025-04-20 19:05:42 -0300

    nutrindo

---
+++ aula4/src/components/Exercicio2.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Exercicio2 = () => {
+  return (
+    <div style = {{color: '#ff0000', backgroundColor: '#0000ff', width: '200px', textAling: 'center'}}>
+        
+        ISSO É UM TESTE
+    </div>
+  )
+}
+
+export default Exercicio2
