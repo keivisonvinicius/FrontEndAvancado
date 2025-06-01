a89395493c42c33139dc3f0f1be1e718027a2473
Keivison <eikeivison@gmail.com>
2025-06-01 15:17:00 -0300

    nutrindo

--- aula6/src/pages/DpoLgpd.jsx
+++ aula6/src/pages/DpoLgpd.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, {useEffect, useState } from 'react'
+import axios from 'axios'
 
 const DpoLgpd = () => {
+
+  const [dpoLgpd, setDpoLgpd] = useState([])
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/dpo-lgpd") //request
+    .then(response => setDpoLgpd(response.data)) //response
+    .catch(error => console.error("Erro ao carregar a lista de dpo-lgpd: ", error))
+  }, [])
+
   return (
-    <div><h3>Essa é a página DPO LGPD</h3></div>
+    <div>
+      <h1>DPO-LGPD</h1>
+      <ul>
+        {
+          dpoLgpd.map((dpoLgpd) => (
+            <li key={dpoLgpd.id}>
+              <h2>{dpoLgpd.titulo}</h2>
+              <p>{dpoLgpd.texto}</p>
+            </li>
+          ))
+        }
+      </ul>
+    </div>
   )
 }
 
