518a10cc4eb3a063a834005eebce5694553e2e2e
Keivison <eikeivison@gmail.com>
2025-06-01 15:02:05 -0300

    nutrindo

--- aula6/src/pages/Noticias.jsx
+++ aula6/src/pages/Noticias.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, {useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Noticias = () => {
+  
+  const [noticias, setNoticias] = useState([])
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/noticias") //request
+    .then(response => setNoticias(response.data)) //response
+    .catch(error => console.error("Erro ao carregar a lista de notícias: ", error))
+  }, [])
+
   return (
-    <div><h3>Essa é a página de Notícias...</h3></div>
+    <div>
+        <h1>Noticias</h1>
+        <ul>
+          {
+          noticias.map((noticia) => (
+            <li key={noticia.id}>
+              <h2>{noticia.titulo}</h2>
+            </li>
+          ))
+          }
+        </ul>
+    </div>
   )
 }
 
